a174567667b84175303c9b2092da6c819352b073
Dylan Lowes <121920809+d-lowes@users.noreply.github.com>
2023-04-04 21:15:42 -0700

    specified dom selector variables, refactored significant figure function for two decimal places, cleaned up comments

--- loan-calculator/calculator.js
+++ loan-calculator/calculator.js
@@ -1,31 +1,21 @@
-//  TODO: add "use strict;" to javascript files
-
 "use strict";
 
 // Find DOM element and hold onto it, so we don't have to search for it
 // every time we use it.
 const calcForm = document.getElementById("calc-form");
-
 const monthlyPaymentResults = document.getElementById("calc-monthly-payment");
-
-// TODO: rename variables to make a distinction between DOM element and number values
-const loanAmount = document.getElementById("loan-amount");
-const loanYears = document.getElementById("loan-years");
-const loanRate = document.getElementById("loan-rate");
+const loanAmountElement = document.getElementById("loan-amount");
+const loanYearsElement = document.getElementById("loan-years");
+const loanRateElement = document.getElementById("loan-rate");
 
 
 
-/** Get form values and return as `{amount, years, rate}`.
- *
- * Example output: `{"amount": 10000, "years": 10, "rate": 4.5}`.
- *
- * */
+/** Get form values and return as `{amount, years, rate}`. */
 
-// TODO: rename variables
 function getFormValues() {
-  const amount = Number(loanAmount.value);
-  const years = Number(loanYears.value);
-  const rate = Number(loanRate.value);
+  const amount = Number(loanAmountElement.value);
+  const years = Number(loanYearsElement.value);
+  const rate = Number(loanRateElement.value);
 
   return { amount, years, rate };
 }
@@ -37,6 +27,7 @@ function calcMonthlyPayment(amount, years, rate) {
   const i = ((rate / 100) / 12);
   const n = (years * 12);
   const P = amount;
+
   const monthlyPayment = ((P * i) / (1 - ((1 + i) ** (-n))));
 
   return monthlyPayment;
@@ -44,7 +35,6 @@ function calcMonthlyPayment(amount, years, rate) {
 
 
 /** Get form values, calculate, convert to 2-decimal places, and update UI. */
-// TODO: remove number()
 // TODO: toFixed(); for decimal places
 
 function getFormValuesAndDisplayResults() {
@@ -55,18 +45,17 @@ function getFormValuesAndDisplayResults() {
   const formRate = formValues.rate;
 
   const result = calcMonthlyPayment(formAmount, formYears, formRate);
-  const resultTwoDecimals = (Math.round(result * 100) / 100);
 
-  monthlyPaymentResults.innerText = resultTwoDecimals;
+  monthlyPaymentResults.innerText = result.toFixed(2);
 }
 
 
 /** Set initial form values and show initial results. Called at app start. */
 
 function setInitialValues() {
-  loanAmount.value = 10000;
-  loanYears.value = 10;
-  loanRate.value = 4.5;
+  loanAmountElement.value = 10000;
+  loanYearsElement.value = 10;
+  loanRateElement.value = 4.5;
 
   getFormValuesAndDisplayResults();
 }
